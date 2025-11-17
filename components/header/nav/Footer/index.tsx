9061e9c3a3a95d3228f33c6fafa9484a8865102c
joneltech07 <jonel.uligan7@gmail.com>
2025-11-17 23:20:20 +0800

    change index to Index in footer component.

--- components/header/nav/Footer/index.tsx
+++ components/header/nav/Footer/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./style.module.scss";
 
-export default function index() {
+export default function Index() {
   return (
     <div className={styles.footer}>
       <p>&copy; 2025 Jonel Tech</p>
