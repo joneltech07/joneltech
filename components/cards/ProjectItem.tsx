d346ada6e5bf90b1297c44de9c5b4811ec730f78
joneltech07 <jonel.uligan7@gmail.com>
2025-11-25 01:32:19 +0800

    make the desc and visuals in column view in project item details.

--- components/cards/ProjectItem.tsx
+++ components/cards/ProjectItem.tsx
@@ -131,7 +131,7 @@ export default function ProjectItem({
               </DialogHeader>
             </VisuallyHidden.Root>
 
-            <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-col md:flex-row flex-wrap justify-center items-center">
               <FadeInOnScroll
                 delay={0.7}
                 yOffset={10}
